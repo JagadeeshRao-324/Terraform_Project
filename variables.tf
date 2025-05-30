3a5edc4a0bc37a31d28ff7152f86397b435b18c6
JagadeeshRao-324 <jagadeesh9440570475@gmail.com>
2025-05-30 19:06:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,27 @@
 variable "rgname" {
 type = string
 }
+
+variable "location" {
+type = string
+}
+
+variable "vnetname" {
+type = string
+}
+
+variable "subnetname" {
+type = string
+}
+
+variable "addr_space" {
+type = list
+}
+
+variable "subnet_prefix" {
+type = list
+}
+
+variable "aksname" {
+type = string
+}
