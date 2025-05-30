1d2d288500f3a11bfd52546bbc8e5bacb89173d3
JagadeeshRao-324 <jagadeesh9440570475@gmail.com>
2025-05-30 19:14:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "AKS_cluster" {
-source = "./modules/AKS_cluster"
-rgname = "aks-rg"
-location = "Central India"
-vnetname = "vnet1"
-subnetname = "subnet1"
-addr_space = ["10.0.0.0/8"]
-addr_prefix = ["10.240.0.0/16"]
-aksname = "cluster1"
+source      = "./modules/AKS_cluster"
+rgname      = var.rgname
+location    = var.location
+vnetname    = var.vnetname
+subnetname  = var.subnetname
+addr_space  = var.addr_space
+addr_prefix = var.addr_prefix
+aksname     = var.aksname
 }
