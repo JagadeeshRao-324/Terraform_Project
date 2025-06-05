2b005e587f8aa4d112685c51ae226cc40d5e1a0b
JagadeeshRao-324 <jagadeesh9440570475@gmail.com>
2025-06-05 17:11:07 +0530

    Update main.tf

--- modules/AKS_cluster/main.tf
+++ modules/AKS_cluster/main.tf
@@ -1,44 +1,71 @@
-
 resource "azurerm_resource_group" "tf-rg" {
-name     = var.rgname
-location = var.location
+  name     = var.rgname
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-name                = var.vnetname
-resource_group_name = var.rgname
-location            = var.location
-address_space       = var.addr_space
+  name                = var.vnetname
+  resource_group_name = azurerm_resource_group.tf-rg.name
+  location            = azurerm_resource_group.tf-rg.location
+  address_space       = var.addr_space
 }
 
 resource "azurerm_subnet" "subnet" {
-name                 = var.subnetname
-resource_group_name  = var.rgname
-virtual_network_name = azurerm_vitual_network.vnet.name
-address_prefixes     = var.subnet_prefix
+  name                 = var.subnetname
+  resource_group_name  = azurerm_resource_group.tf-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.addr_prefix
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-name                = var.aksname
-resource_group_name = var.rgname
-dns_prefix          = var.aksname
-kubernetes_version  = 1.31.0
+  name                = var.aksname
+  location            = azurerm_resource_group.tf-rg.location
+  resource_group_name = azurerm_resource_group.tf-rg.name
+  dns_prefix          = var.aksname
+  kubernetes_version  = "1.32.3"
 
-default_node_pool {
-  name           = "nodepool"
-  vm_size        = "Standard_DS2_v2"
-  node_count     = 2
-  vnet_subnet_id = azurerm_subnet.subnet.id
- }
+  default_node_pool {
+    name           = "nodepool"
+    vm_size        = "Standard_DS2_v2"
+    node_count     = 2
+    vnet_subnet_id = azurerm_subnet.subnet.id
+  }
 
-identity {
-  type = "SystemAssigned"
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
 }
 
-network_profile {
-  network_plugin = "azure"
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
